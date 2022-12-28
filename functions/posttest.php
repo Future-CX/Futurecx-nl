cca9679190ed4339a32ae5773119ebd139bf9110
Future CX <m.van.deel@futurecx.nl>
2022-12-28 17:12:55 +0100

    posttest

---
+++ functions/posttest.php
@@ -0,0 +1,14 @@
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+  foreach ($_POST as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
+  
+}
