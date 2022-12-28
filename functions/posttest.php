96c0855735e8b569525aa2d69bbc81afd280ff34
Future CX <m.van.deel@futurecx.nl>
2022-12-28 17:37:06 +0100

    postttest

--- functions/posttest.php
+++ functions/posttest.php
@@ -1,16 +1,33 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
-  foreach ($_POST as $key => $value) {
-        echo "<tr>";
-        echo "<td>";
-        echo $key;
-        echo "</td>";
-        echo "<td>";
-        echo $value;
-        echo "</td>";
-        echo "</tr>";
+  	$date = date('m/d/Y h:i:s a', time());
+	
+	$headers = 'From: webmaster@example.com'       . "\r\n" .
+                 'Reply-To: webmaster@example.com' . "\r\n" .
+					 'X-Mailer: PHP/' . phpversion();
+	$to      = 'm.van.deel@futurecx.nl';
+    $subject = 'PostTest at ' + $date;
+    $message = '';
+	
+	foreach ($_POST as $key => $value) {
+        $message += "<tr>";
+        $message += "<td>";
+        $message += $key;
+        $message += "</td>";
+        $message += "<td>";
+        $message += $value;
+        $message += "</td>";
+        $message += "</tr>";
     }
+    
+
+    mail($to, $subject, $message, $headers);
+	
+	
+	
+	
+	
   
 }
 ?>
