5795db090b7b459a065ee7ba4431b3648fff3912
Future CX <m.van.deel@futurecx.nl>
2022-12-28 21:42:42 +0100

    posttest

--- functions/posttest.php
+++ functions/posttest.php
@@ -3,12 +3,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
   	$date = date('m/d/Y h:i:s a', time());
 	
-	$headers = 'From: webmaster@futurecx.nl'       . "\r\n" .
-                 'Reply-To: webmaster@futurecx.nl' . "\r\n" .
-					 'X-Mailer: PHP/' . phpversion();
+	$headers = "MIME-Version: 1.0" . "\r\n"; 
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+	// Additional headers 
+	$headers .= 'From: Future CX<webmaster@futurecx.nl>' . "\r\n"; 
+	//$headers .= 'Cc: welcome@example.com' . "\r\n"; 
+	//$headers .= 'Bcc: welcome2@example.com' . "\r\n"; 
+	
 	$to      = 'm.van.deel@futurecx.nl';
     $subject = 'PostTest at ' . $date;
-    $message = 'POST PARAMETERS';
+    
+	$message = '<html><head><title>Future CX Post Test</title></head><body>';
+	
+	$message .= '<h2>POST PARAMETERS</h2>';
 	
 	foreach ($_POST as $key => $value) {
         $message .= "<tr>";
@@ -21,15 +28,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $message .= "</tr>";
     }
 	
-	$message .= 'JSON BODY';
-	$message .= "<pre>";
+	$message .= '<h2>JSON BODY</h2>';
+	
 	
 	$data = json_decode(file_get_contents('php://input'), true);
+	
+	$message .= "<pre>";
 	
 	$message .= json_encode($data, JSON_PRETTY_PRINT);
 	
 	$message .= "</pre>";
-    
+	
+	
+	
+    $message .= "</body></html>";
 
     mail($to, $subject, $message, $headers);
 	
